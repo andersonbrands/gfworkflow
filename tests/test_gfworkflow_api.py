67883fe0d18e5731d956233d5e2027e5d11fdcee
Anderson Brandao <anderson.brands@gmail.com>
2020-10-25 23:52:59 -0300

    Generalize test to cover multiple api methods

--- tests/test_gfworkflow_api.py
+++ tests/test_gfworkflow_api.py
@@ -1,9 +1,18 @@
+from functools import partial
 from unittest import mock
 
-from gfworkflow.cli.api import clear_log_method
+import pytest
+
+from gfworkflow.cli.api import clear_log_method, dump_log_method
 
 
-def test_clear_log_method_calls_clear_log():
-    with mock.patch('gfworkflow.clear_log') as _clear_log:
-        clear_log_method()
-        _clear_log.assert_called_once()
+@pytest.mark.parametrize(
+    'api_method, matching_method', (
+            (clear_log_method, 'gfworkflow.clear_log'),
+            (partial(dump_log_method, '.'), 'gfworkflow.dump_log'),
+    )
+)
+def test_api_method_calls_matching_method(api_method, matching_method):
+    with mock.patch(matching_method) as _patched:
+        api_method()
+        _patched.assert_called_once()
