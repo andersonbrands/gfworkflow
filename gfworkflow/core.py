70c33b6a8e801d3420d20c66211b9c64b361c412
Anderson Brandao <anderson.brands@gmail.com>
2020-11-01 23:34:19 -0300

    Test get current branch name

--- gfworkflow/core.py
+++ gfworkflow/core.py
@@ -28,3 +28,7 @@ def start_release(new_version: str):
 def get_new_version(part: str):
     output = run(f'bumpversion {part} --list -n --allow-dirty --no-configured-files').stdout
     return re.compile(r'new_version=(\S+)').search(output).group(1)
+
+
+def get_current_branch_name():
+    return run('git rev-parse --abbrev-ref HEAD').stdout.strip()
